3471bf52f152e39d3bfa93170a5404d8449afd8c
Yease <yease@foxmail.com>
2016-03-28 00:26:25 +0800

    Create struct_list.c

---
+++ struct_list.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+struct tt
+{
+	int x;
+	struct *y;
+}*p;
+struct tt a[4]={20,a+1,15,a+2,30,a+3,17,a};
+
+main()
+{
+	int i;
+	p = a;
+	for(i=1 ; i<=2 ; i++)
+	{
+		printf("%d,",p->x);
+		p->y;
+	}
+	printf("\n");
+}
